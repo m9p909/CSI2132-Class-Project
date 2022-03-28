90ffb4c9f7841c10eb76ea6a5f39243c8184055f
Victor Babineau <vbabi020@uottawa.ca>
2022-03-28 17:01:54 -0400

    Finished the dentist_branch_form.html it looks bad tho

--- dentistapp/dentistapp/views/showDentistsInBranch.py
+++ dentistapp/dentistapp/views/showDentistsInBranch.py
@@ -11,31 +11,36 @@ class Dentist(Form):
 
 
 def form_to_dict(form: Form) -> dict:
-    res = {}
+    res={}
+    subres = {}
+    for key in form.keys():
+        subres[key]=[]
     for row in form:
+        for key in form.keys():
+            subres[key].append(row[key])
     # make form into dict
+    res['query']=subres
     return res
 
 def query_dentist():
     with get_engine().connect() as conn:
 
         query = text(
-            "SELECT employee_id , d.branch_id FROM dentist d, branch b WHERE d.branch_id=b.branch_id;",
+            "SELECT p.f_name,p.l_name, employee_id , d.branch_id,b.city FROM person p, dentist d, branch b WHERE d.branch_id=b.branch_id and p.person_id=employee_id;",
         )
 
         result = conn.execute(query)
-        for row in result:
-            print("Dentist ID:", row['employee_id'])
-            print("Branch ID:", row['branch_id'])
+        # for row in result:
+        #     print("First and Last name:", row['f_name'],row['l_name'])
+        #     print("Dentist ID:", row['employee_id'])
+        #     print("Branch ID:", row['branch_id'])
         return result
 
 def get_dentist_branch(request):
     if request.method == "GET":
         result = query_dentist()
-        print(result)
-        result = form_to_dict(result)
-
-        return render(request, "dentist_branch_form.html", result)
+        context = form_to_dict(result)
+        return render(request, "dentist_branch_form.html", context)
 
 def dentist_in_branch_endpoint(request):
     return get_dentist_branch(request)
