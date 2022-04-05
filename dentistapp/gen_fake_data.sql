24a8a4743edaca79001fcd77de5963456b094176
Carson Boyne <cboyn084@uottawa.ca>
2022-04-05 13:17:24 -0400

    updated fake data

--- dentistapp/gen_fake_data.sql
+++ dentistapp/gen_fake_data.sql
@@ -33,9 +33,9 @@ values (321,
         'Male',
         '1111111');
 INSERT into employee
-values (123, 1000000);
+values (321, 1000000);
 insert into dentist
-values (123);
+values (321);
 
 
 
@@ -44,3 +44,36 @@ values ('cool city', 123);
 
 insert into branch(city, manager_id)
 values ('bad city', 123);
+
+
+--Create patient--
+insert into person
+values (555,
+        '02 / 14 / 10',
+        'patient',
+        'frog',
+        'the future',
+        12,
+        'potato street',
+        '111111',
+        'ON',
+        'adsa@ad.com',
+        'Male',
+        '1111111');
+
+INSERT INTO patient
+VALUES(555,'111111');
+
+
+INSERT INTO Appointment_procedure(procedure_type)
+VALUES ('Scaling');
+
+INSERT INTO Appointment_procedure(procedure_type)
+VALUES ('Fluoride');
+
+INSERT INTO Appointment_procedure(procedure_type)
+VALUES ('Removal');
+
+INSERT INTO Appointment_procedure(procedure_type)
+VALUES ('Cavity Filling');
+
